b81846acad5e65fc81b9f841d0349083a414b7a6
Kathryn Newbould <kathrynrnewbould@gmail.com>
2019-06-08 10:39:48 +0200

    Added images from goodreads via CORS server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Media from "react-bootstrap/Media";
+import axios from "axios";
 import "./App.css";
 import bookdata from "./data/books.json";
 
@@ -12,24 +14,73 @@ function App() {
 }
 
 function generateBookList(booksList) {
-  console.log(process.env);
   return (
     <ul>
       {booksList.map(item => (
-        <GetBookInfo book={item} key={item.title} />
+        <GetBookInfo goodreads_id={item.goodreads_id} key={item.goodreads_id} />
       ))}
     </ul>
   );
 }
 
 class GetBookInfo extends Component {
+  state = {
+    isBookLoaded: false,
+    book: {}
+  };
+
+  componentDidMount() {
+    this.getBookInfo(this.props.goodreads_id);
+  }
+
   render() {
-    const book = this.props.book;
-    return (
-      <li>
-        {book.title} by {book.author}
-      </li>
-    );
+    const { isBookLoaded, book } = this.state;
+    if (isBookLoaded) {
+      return (
+        <Media>
+          {this.getImage()}
+          {book.title} by {book.authors.author.name}
+        </Media>
+      );
+    }
+    return <div />;
+  }
+
+  getImage() {
+    const { isBookLoaded, book } = this.state;
+    if (isBookLoaded) {
+      return (
+        <img
+          width={64}
+          height={64}
+          className="mr-3"
+          src={book.image_url}
+          alt={book.title}
+        />
+      );
+    }
+  }
+
+  getBookInfo(goodreads_id) {
+    axios
+      .get(
+        "http://ec2-52-70-122-182.compute-1.amazonaws.com:3001/getbookinfo?grid=" +
+          goodreads_id
+      )
+      .then(
+        result => {
+          this.setState({
+            isBookLoaded: true,
+            book: JSON.parse(result.data).GoodreadsResponse.book
+          });
+        },
+        error => {
+          console.log(error);
+          this.setState({
+            isBookLoaded: false
+          });
+        }
+      );
   }
 }
 
