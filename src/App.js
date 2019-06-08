11137f1a62eaffc799bad874ab23cd950c2d3ef8
Kathryn Newbould <kathrynrnewbould@gmail.com>
2019-06-08 06:38:23 +0200

    Migrated older stuff to reactjs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import bookdata from './data/books.json';
 
 function App() {
   return (
     <div className="App">
       <h1>A List of Indie Books!</h1>
+      {generateBookList(bookdata.books)}
     </div>
   );
+}
+
+function generateBookList(booksList) {
+	let list;
+	for (var index in booksList) {
+    console.log(<GetBookInfo book={booksList[index]}/>)  // (index, booksList)
+	}
+	return  <ul> {
+    booksList.map(item => (
+    <GetBookInfo book={item}/>
+  ))
+} </ul>;
+}
+
+class GetBookInfo extends Component {
+	render() {
+    const book = this.props.book;
+  	return (<li>
+  		+ {book.title}
+  		+ " by "
+  		+ {book.author}
+  		+ </li>)
+    }
 }
 
 export default App;
